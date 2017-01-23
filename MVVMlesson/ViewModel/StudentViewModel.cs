d349e0c9f78ff8ac3c64a0114bda8be81b16d613
dona0166 <dordattoli@gmail.com>
2017-01-23 09:30:29 +0100

    bla bla bla
    uploaded

---
+++ MVVMlesson/ViewModel/StudentViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMlesson.Model;
+using System.Collections.ObjectModel;
+
+
+namespace MVVMlesson.ViewModel
+{
+    class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = students;
+
+        }
+    }
+}
